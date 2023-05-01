e4ad3ce4ead48c4c3bcfdd34ac64ca25ca75e77f
Heiner000 <garrett.heiner@gmail.com>
2023-05-01 15:48:08 -0700

    adds li for garrett

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,18 +5,16 @@ export default function About() {
             <ul> 
                 <li>Egor</li>
                 <li><img src={process.env.PUBLIC_URL + "/owlavatar.png"} /></li>
-
-                    
             </ul>
           
             <ul> 
                 <li>Garrett</li>
                 <li><img src={process.env.PUBLIC_URL + "/bearavatar.jpeg"} /></li>
-
+                <li>Food & Hibernation Enjoyer</li>
             </ul>
             <ul> 
                 <li>Gloree</li>
-               
+                <li><img src={process.env.PUBLIC_URL + "/spiritcat.png"} /></li>
                     <p>Cat and Pasta Lover</p>
             </ul>
         </>
