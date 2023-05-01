45262a21901ba24fa86f51b99988f88cd32f1bed
gloreea <aranda_gloree408@icloud.com>
2023-05-01 15:03:16 -0700

    header issue resolved

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,12 +3,15 @@ export default function About() {
         <>
             <ul> 
                 <li>Egor</li>
+                    <p></p>
             </ul>
             <ul> 
                 <li>Garrett</li>
+                    <p></p>
             </ul>
             <ul> 
                 <li>Gloree</li>
+                    <p>Cat and Pasta Lover</p>
             </ul>
         </>
     )
